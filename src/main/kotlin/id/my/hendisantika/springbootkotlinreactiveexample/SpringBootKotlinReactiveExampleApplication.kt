616b394d290a863064631fc1fda4f1ba9deaf27e
hendisantika <hendisantika@yahoo.co.id>
2024-04-20 08:45:13 +0700

    Add RegisterReflectionForBinding 🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/springbootkotlinreactiveexample/SpringBootKotlinReactiveExampleApplication.kt
+++ src/main/kotlin/id/my/hendisantika/springbootkotlinreactiveexample/SpringBootKotlinReactiveExampleApplication.kt
@@ -1,8 +1,17 @@
 package id.my.hendisantika.springbootkotlinreactiveexample
 
+import id.my.hendisantika.springbootkotlinreactiveexample.dto.ErrorDTO
+import id.my.hendisantika.springbootkotlinreactiveexample.dto.NewsDTO
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@RegisterReflectionForBinding(
+    classes = [
+        NewsDTO::class,
+        ErrorDTO::class
+    ]
+)
 @SpringBootApplication
 class SpringBootKotlinReactiveExampleApplication
 
