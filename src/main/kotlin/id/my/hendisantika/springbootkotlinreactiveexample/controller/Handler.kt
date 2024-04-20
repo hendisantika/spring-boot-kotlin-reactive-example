e832443d85587ef409d86d33f3137a4848193dec
hendisantika <hendisantika@yahoo.co.id>
2024-04-20 08:47:43 +0700

    Add getNews in Handler 🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/springbootkotlinreactiveexample/controller/Handler.kt
+++ src/main/kotlin/id/my/hendisantika/springbootkotlinreactiveexample/controller/Handler.kt
@@ -1,14 +1,13 @@
 package id.my.hendisantika.springbootkotlinreactiveexample.controller
 
 import id.my.hendisantika.springbootkotlinreactiveexample.data.toDTO
+import id.my.hendisantika.springbootkotlinreactiveexample.dto.ErrorDTO
 import id.my.hendisantika.springbootkotlinreactiveexample.repository.NewsRepository
 import kotlinx.coroutines.flow.map
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.*
 
 @Component
 class Handler(
@@ -23,5 +22,21 @@ class Handler(
             .ok()
             .contentType(MediaType.APPLICATION_JSON)
             .bodyAndAwait(news)
+    }
+
+            suspend fun getNews(req: ServerRequest): ServerResponse {
+        val id = req.pathVariable(NEWS_ID).toLongOrNull() ?: return ServerResponse
+            .badRequest()
+            .bodyValue(ErrorDTO(msg = "Invalid news id in path"))
+            .awaitSingle()
+
+        val news = repository.findById(id)?.toDTO() ?: return ServerResponse
+            .notFound()
+            .buildAndAwait()
+
+        return ServerResponse
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValueAndAwait(news)
     }
 )
