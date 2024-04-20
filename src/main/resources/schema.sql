0368c301988c1365791d2681a56a907848553d98
hendisantika <hendisantika@yahoo.co.id>
2024-04-20 08:40:43 +0700

    Add schema.sql file 🫸🌀✏️📗🐧🐳

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE if NOT EXISTS news
+(
+    id
+    BIGINT
+    NOT
+    NULL
+    AUTO_INCREMENT
+    PRIMARY
+    KEY,
+    title
+    VARCHAR
+(
+    255
+) NOT NULL,
+    text VARCHAR
+(
+    255
+) NOT NULL,
+    url VARCHAR
+(
+    255
+) NOT NULL,
+    image_url VARCHAR
+(
+    255
+)
+    );
