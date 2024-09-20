e26f216bc1a41073e20a1991182af74572c817f8
Hendi Santika <hendisantika@yahoo.co.id>
2024-09-20 08:18:24 +0700

    Upgraded into Spring Boot 3.2.8 version 🫸🌀✏️📗🐧🐳⬆
    Upgraded into Spring Boot 3.2.8 version 🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val kotlin_version = "1.9.23" // also update plugin versions
 val kotlin_serialization_version = "1.4.0"
 
 plugins {
-    id("org.springframework.boot") version "3.2.7"
+    id("org.springframework.boot") version "3.2.8"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.graalvm.buildtools.native") version "0.9.17"
     kotlin("jvm") version "1.9.23"
